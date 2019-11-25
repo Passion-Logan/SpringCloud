da8403cb92e67a422366c9c668b37d5d4e343bb7
passion-logan <wangdadamailbox@163.com>
2019-11-25 18:03:49 +0800

    添加feign服务消费端

--- eureka-consumer-feign/src/main/java/com/cody/controller/DcController.java
+++ eureka-consumer-feign/src/main/java/com/cody/controller/DcController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * 而在具体使用的时候就跟调用本地方法一点的进行调用即可
  * 由于Feign是基于Ribbon实现的，所以它自带了客户端负载均衡功能，也可以通过Ribbon的IRule进行策略扩展
  * 另外，Feign还整合的Hystrix来实现服务的容错保护，在Dalston版本中，Feign的Hystrix默认是关闭的
+ * 配置文件中配置 eureka.feign.hystrix.enabled=true 可开启Hystrix
  * <p>
  * Copyright: Copyright (C) 2019 XXX, Inc. All rights reserved. <p>
  *
